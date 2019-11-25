91cfdd2ea981720d202afb6070152e65823a9e11
José Cleanto <josecleanto@gmail.com>
2019-11-25 11:09:12 +0000

    adjust button text and link call css

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function changeButton(){
   var objButton = document.getElementById("btnDonate");
-  objButton.innerText = "Already donated";
+  objButton.innerText = "Donated";
   objButton.className = "redButton";
   objButton.disabled = "disabled";
   alert("Thank you for your donation!");
